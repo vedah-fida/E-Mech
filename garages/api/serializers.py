fdddc958baf2f1c8c2fbe051909f4c0df22d2d64
vedah-fida <vedahfida@gmail.com>
2018-03-21 20:14:42 +0300

    Id added to Json

--- garages/api/serializers.py
+++ garages/api/serializers.py
@@ -5,6 +5,7 @@ class GaragesSerializer(serializers.ModelSerializer):
     class Meta:
         model = RegisterGarage
         fields = (
+            "id",
             "garage_name",
             "garage_location",
             "garage_email",
